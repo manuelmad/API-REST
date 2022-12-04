6599e575c0394b97553ab599bdae185ad3b1357f
Manuel Araujo <manuelmad1981@gmail.com>
2022-12-04 17:52:05 -0400

    Añadiendo interactividad con botón de flecha y el hash. Consumiendo datos de serie de Sonic

--- src/main.js
+++ src/main.js
@@ -2,47 +2,112 @@
 const API_URL = 'https://api.rawg.io/api';
 
 // EJEMPLO DE URL QUE CONTIENE A LOS JUEGOS EN PÁGINAS DE 30 JUEGOS CADA UNA Y ACCEDO A LA PÁGINA 2
-const API_URL_GAMES = 'https://api.rawg.io/api/games/super-mario-bros?key=8d35f3d37cff4c7c816bce53458b3cb4';
+const API_URL_SONIC_SERIES = 'https://api.rawg.io/api/games/sonic-the-hedgehog/game-series?key=8d35f3d37cff4c7c816bce53458b3cb4';
+
+// const API_URL_GAME = 'https://api.rawg.io/api/games/sonic-3-and-knuckles?key=8d35f3d37cff4c7c816bce53458b3cb4'
 
 // EJEMPLO DE URL QUE CONTIENE A LOS GÉNEROS
-const API_URL_GENRES = 'https://api.rawg.io/api/genres?key=8d35f3d37cff4c7c816bce53458b3cb4';
+// const API_URL_GENRES = 'https://api.rawg.io/api/genres?key=8d35f3d37cff4c7c816bce53458b3cb4';
 
+let sonic_series = [];
+// let playED_games = [];
+// let playING_games = [];
+// let TOplay_games = [];
+let new_url;
+let res2;
+let data2;
 
-async function showGames() {
-    const res = await fetch(API_URL_GAMES);
+async function showSonicSeries() {
+    const res = await fetch(API_URL_SONIC_SERIES);
     const data = await res.json();
 
     if(res.status !== 200) {
         console.log("Hubo un error: " + res.status);
     } else {
+        sonic_series.push(data.results);
         console.log('Games', data);
-        const img2 = document.getElementById('img2');
-        img2.src = data.background_image;
+        console.log('Página 1', data.results);
+        new_url = data.next;
+        for(let i = 1; i <= (data.count)/10; i++) {
+            // console.log('Games', data.results);
+            res2 = await fetch(new_url);
+            data2 = await res2.json();
+            console.log('Página '+(i+1), data2.results);
+            sonic_series.push(data2.results);
+            new_url = data2.next;
+        }
+    }
+    sonic_series = sonic_series.flat();
+    console.log('Sonic Series', sonic_series);
+    showPlayedGames();
+}
+
+showSonicSeries();
+
+function showPlayedGames() {
+    for(let i=0; i < playED_games.length; i++) {
+        sonic_series.forEach(item => {
+            if(item.slug == playED_games[i]) {
+                const div = document.createElement('div');
+                const img = document.createElement('img');
+                img.src = item.background_image;
+                img.width =  140;
+                const span = document.createElement('span');
+                const spanText = document.createTextNode(item.name);
+                
+                span.appendChild(spanText);
+                div.appendChild(img);
+                div.appendChild(span);
+                playedGamesContainer.appendChild(div);
+            }
+        });
     }
 }
 
-showGames();
+// showPlayedGames();
+
+// sonic_series[0].played = true;
 
 
-async function showGenres() {
-    const res = await fetch(API_URL_GENRES);
-    const data = await res.json();
 
-    if(res.status !== 200) {
-        console.log("Hubo un error: " + res.status);
-    } else {
-        console.log('Genres', data);
+// async function showGame() {
+//     const res = await fetch(API_URL_GAME);
+//     const data = await res.json();
 
-        const platforms = data.results[10];
-        console.log('Platforms', platforms);
-        const games = platforms.games;
-        console.log('Game', games);
-        const mario = games.find(item => item.name == 'Super Meat Boy');
-        console.log('Super Meat Boy', mario);
-    }
-}
+//     if(res.status !== 200) {
+//         console.log("Hubo un error: " + res.status);
+//     } else {
+//         console.log('Games', data);
+//         const img2 = document.getElementById('img2');
+//         img2.src = data.background_image;
+//         const span2 = document.getElementById('span2');
+//         span2.innerHTML = data.name;
+//         console.log(data.results);
+//     }
+// }
 
-showGenres();
+// showGame();
+
+
+// async function showGenres() {
+//     const res = await fetch(API_URL_GENRES);
+//     const data = await res.json();
+
+//     if(res.status !== 200) {
+//         console.log("Hubo un error: " + res.status);
+//     } else {
+//         console.log('Genres', data);
+
+//         const platforms = data.results[10];
+//         console.log('Platforms', platforms);
+//         const games = platforms.games;
+//         console.log('Game', games);
+//         const mario = games.find(item => item.name == 'Super Meat Boy');
+//         console.log('Super Meat Boy', mario);
+//     }
+// }
+
+// showGenres();
 
 
 /* Notas */
---
+++ src/navegacion.js
@@ -0,0 +1,79 @@
+played_button.addEventListener('click', () => {
+    location.hash = '#played';
+});
+
+arrowButton.addEventListener('click', () => {
+    history.back();
+    //location.hash = '#home';
+});
+
+window.addEventListener('DOMContentLoaded', navigator, false);
+window.addEventListener('hashchange', navigator, false);
+
+function navigator() {
+    console.log({location});
+
+    if(location.hash.startsWith('#played')) {
+        playedPage();
+    }
+    // else if(location.hash.startsWith('#search=')) {
+    //     searchPage();
+    // }
+    // else if(location.hash.startsWith('#movie=')) {
+    //     movieDetailsPage();
+    // }
+    // else if(location.hash.startsWith('#category=')) {
+    //     categoriesPage();
+    // }
+    else {
+        homePage();
+    }
+
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function homePage() {
+    main.style.height = '90vh';
+    header.style.background = '#fe0100';
+    // headerSection.classList.remove('header-container--long');
+    // headerSection.style.background = '';
+    arrowButton.classList.add('inactive');
+    // arrowBtn.classList.remove('header-arrow--white');
+    // headerTitle.classList.remove('inactive');
+    // headerCategoryTitle.classList.add('inactive');
+    // searchForm.classList.remove('inactive');
+
+    mainSectionContainer.classList.remove('inactive');
+    // categoriesPreviewSection.classList.remove('inactive');
+    playedSectionContainer.classList.add('inactive');
+    // movieDetailSection.classList.add('inactive');
+
+    // getTrendingMoviesPreview();
+    // getCategoriresPreview();
+    console.log('Home!!!');
+    location.hash = '#home';
+}
+
+function playedPage() {
+    console.log('PLAYED!!!');
+    header.style.background = '#105001';
+    main.style.height = 'auto';
+    mainSectionContainer.classList.add('inactive');
+    // headerSection.classList.remove('header-container--long');
+    // headerSection.style.background = '';
+    arrowButton.classList.remove('inactive');
+    // arrowBtn.classList.remove('header-arrow--white');
+    // headerTitle.classList.add('inactive');
+    playedSectionContainer.classList.remove('inactive');
+    // searchForm.classList.add('inactive');
+
+    // trendingPreviewSection.classList.add('inactive');
+    // categoriesPreviewSection.classList.add('inactive');
+    // genericSection.classList.remove('inactive');
+    // movieDetailSection.classList.add('inactive');
+
+    // headerCategoryTitle.innerHTML = 'Tendencias';
+
+    // getTrendingMovies();
+}
