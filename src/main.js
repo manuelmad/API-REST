a6bedcf6d5a69946fd98c37c4b9514eff859e60a
Manuel Araujo <manuelmad1981@gmail.com>
2022-12-05 09:48:39 -0400

    Mejoras en el algoritmo

--- src/main.js
+++ src/main.js
@@ -1,35 +1,52 @@
 // URL GENERAL
 const API_URL = 'https://api.rawg.io/api';
 
-// EJEMPLO DE URL QUE CONTIENE A LOS JUEGOS EN PÁGINAS DE 30 JUEGOS CADA UNA Y ACCEDO A LA PÁGINA 2
+// URL DEL JUEGO SONIC DE HEDGEHOG (ORIGINAL)
+const API_URL_SONIC = 'https://api.rawg.io/api/games/53551?key=8d35f3d37cff4c7c816bce53458b3cb4';
+
+// URL QUE CONTIENE A LOS JUEGOS QUE FORMAN PARTE DE LA SERIE DE SONIC (ORIGINAL)
 const API_URL_SONIC_SERIES = 'https://api.rawg.io/api/games/sonic-the-hedgehog/game-series?key=8d35f3d37cff4c7c816bce53458b3cb4';
 
-// const API_URL_GAME = 'https://api.rawg.io/api/games/sonic-3-and-knuckles?key=8d35f3d37cff4c7c816bce53458b3cb4'
+// const API_URL_GAME = 'https://api.rawg.io/api/games/sonic-3-and-knuckles?key=8d35f3d37cff4c7c816bce53458b3cb4';
 
 // EJEMPLO DE URL QUE CONTIENE A LOS GÉNEROS
 // const API_URL_GENRES = 'https://api.rawg.io/api/genres?key=8d35f3d37cff4c7c816bce53458b3cb4';
 
+const API_URL_SEARCH = 'https://api.rawg.io/api/games?search=sonic-the-hedgehog-2&key=8d35f3d37cff4c7c816bce53458b3cb4';
+
+// Array que contendrá todos los juegos de la serie de Sonic
 let sonic_series = [];
 // let playED_games = [];
 // let playING_games = [];
 // let TOplay_games = [];
+
+// Variables para recorrer todas las páginas de la serie de Sonic
 let new_url;
 let res2;
 let data2;
 
+// Fución para crear la lista de juegos de la serie Sonic
 async function showSonicSeries() {
+    // Accedo a la serie de Sonic, usando al juego original como referencia (Éste no aparecerá en la serie)
     const res = await fetch(API_URL_SONIC_SERIES);
     const data = await res.json();
+
+    // Accedo al juego de Sonic Original (id)
+    const res3 = await fetch(API_URL_SONIC);
+    const data3 = await res3.json();
 
     if(res.status !== 200) {
         console.log("Hubo un error: " + res.status);
     } else {
+        // Incluyo los 10 juegos de la primera página de la serie de Sonic en el array
         sonic_series.push(data.results);
-        console.log('Games', data);
         console.log('Página 1', data.results);
+
+        // Accedo a la siguiente página de la serie
         new_url = data.next;
+
+        // Ciclo que recorre las (62/10 = 6.2 => aprox. 6) páginas faltantes de la serie, accede a cada lista de juegos y los agregar al array
         for(let i = 1; i <= (data.count)/10; i++) {
-            // console.log('Games', data.results);
             res2 = await fetch(new_url);
             data2 = await res2.json();
             console.log('Página '+(i+1), data2.results);
@@ -37,13 +54,21 @@ async function showSonicSeries() {
             new_url = data2.next;
         }
     }
+
+    // Agrego el juego original al array de último para mantener el orden por "released"
+    sonic_series.push(data3);
+
+    // Aplano el array de arrays (cada página es una array) para que sea un array de objetos
     sonic_series = sonic_series.flat();
     console.log('Sonic Series', sonic_series);
+
+    // Invoco la función que renderiza los playED Games
     showPlayedGames();
 }
 
 showSonicSeries();
 
+// Función que muestra los playED Games
 function showPlayedGames() {
     for(let i=0; i < playED_games.length; i++) {
         sonic_series.forEach(item => {
@@ -65,10 +90,6 @@ function showPlayedGames() {
 }
 
 // showPlayedGames();
-
-// sonic_series[0].played = true;
-
-
 
 // async function showGame() {
 //     const res = await fetch(API_URL_GAME);
@@ -108,6 +129,19 @@ function showPlayedGames() {
 // }
 
 // showGenres();
+
+async function searchGame() {
+    const res = await fetch(API_URL_SEARCH);
+    const data = await res.json();
+
+    if(res.status !== 200) {
+        console.log("Hubo un error: " + res.status);
+    } else {
+        console.log('Search', data.results);
+    }
+}
+
+searchGame();
 
 
 /* Notas */
